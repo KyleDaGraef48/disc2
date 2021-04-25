a84182ef1c9bfdaa57a64ec35793857505dcacc8
KyleDaGraef48 <kyle.j.graef48@gmail.com>
2021-04-25 15:12:25 -0700

    Update circle.cpp

--- circle.cpp
+++ circle.cpp
@@ -1,15 +1,28 @@
 #include <iostream>
 using namespace std;
 
-//This program prompts user to enter circle raduis
-//Then the circle area is displayed
+
 
 int main()
 {
-	double radius;
-	cout << "Enter radius\n";
-	//Add code to read user input and store in variable radius
-	cout << "The area of a circle with radius \n";
-	//Calculate the area of the circle
+	// display variables and constants
+	double a;
+	double area;
+	const double PI = 3.14159;
+		
+	 // prompt user to input radius 
+	cout << "enter the radius of a circle";
+	cin >> a;
+	
+	// compute area
+	area = pow(a,2) * PI;	
+		
+        // display the area 
+		
+	cout << "the area of the ellipse is " << area;
+	
+	
+	
 	return 0;
+	
 }
